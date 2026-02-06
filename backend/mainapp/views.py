2c1552df007aff5e8aa4fa8910e258d02d80bdf6
kibetnathan <kibetnat@gmail.com>
2026-02-06 09:22:52 +0300

    Make Leadership team API view

--- backend/mainapp/views.py
+++ backend/mainapp/views.py
@@ -7,3 +7,42 @@ from .serializers import LeadershipTeamSerializer, AgeGroupSerializer,Disciplesh
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+class LeadershipTeamView(APIView):
+
+    def get(self, request, *args, **kwargs):
+        result = LeadershipTeam.objects.all()
+        serializers = LeadershipTeamSerializer(result, many=True)
+        return Response({'status': 'success', "leadership team": serializers.data}, status=200)
+    
+    def post(self, request):
+        serializer = LeadershipTeamSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+        else:
+            return Response({"status": "error", "data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+        
+    def put(self, request, *args, **kwargs):
+        try:
+            team = LeadershipTeam.objects.get(id=kwargs['id'])
+        except LeadershipTeam.DoesNotExist:
+            return Response({"status": "error", "data": "Team not found"}, status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = LeadershipTeamSerializer(team, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+        else:
+            return Response({"status": "error", "data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+        
+    def delete(self, request, *args, **kwargs):
+        try:
+            team = LeadershipTeam.objects.get(id=kwargs['id'])
+        except LeadershipTeam.DoesNotExist:
+            return Response({"status": "error", "data": "team not found"}, status=status.HTTP_404_NOT_FOUND)
+        
+        team.delete()
+        return Response({"status": "success", "data": "team deleted"}, status=status.HTTP_200_OK)
+    
+    
