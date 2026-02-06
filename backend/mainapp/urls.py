49eddd8ee02252195768355727b8cf8c7b391083
kibetnathan <kibetnat@gmail.com>
2026-02-06 11:03:49 +0300

    create routers

--- backend/mainapp/urls.py
+++ backend/mainapp/urls.py
@@ -1,13 +1,24 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
-from .views import LeadershipTeamView, AgeGroupView, DiscipleshipGroupView, RopesClassView, ServingTeamView, MinistryDataView
+from .views import LeadershipTeamViewSet, AgeGroupViewSet, DiscipleshipGroupViewSet, RopesClassViewSet, ServingTeamViewSet, MinistryDataViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r"leadership-team", LeadershipTeamViewSet)
+router.register(r"age-group", AgeGroupViewSet)
+router.register(r"discipleship-group", DiscipleshipGroupViewSet)
+router.register(r"ropes-class", RopesClassViewSet)
+router.register(r"serving-team", ServingTeamViewSet)
+router.register(r"ministry-data", MinistryDataViewSet)
+
 
 urlpatterns= [
     path('', views.index, name='index'),
-    path('leadership-teams', LeadershipTeamView.as_view()),
-    path('age-groups', AgeGroupView.as_view()),
-    path('discipleship-groups', DiscipleshipGroupView.as_view()),
-    path('ropes-classes', RopesClassView.as_view()),
-    path('ministry-data', MinistryDataView.as_view()),
-    path('serving-team', ServingTeamView.as_view()),
+    path('', include(router.urls))
+    # path('leadership-teams', LeadershipTeamView.as_view()),
+    # path('age-groups', AgeGroupView.as_view()),
+    # path('discipleship-groups', DiscipleshipGroupView.as_view()),
+    # path('ropes-classes', RopesClassView.as_view()),
+    # path('ministry-data', MinistryDataView.as_view()),
+    # path('serving-team', ServingTeamView.as_view()),
 ]
