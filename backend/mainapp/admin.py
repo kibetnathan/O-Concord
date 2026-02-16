b6dc36183e95f612c2bf3b0ad821b736e50dea6d
kibetnathan <kibetnat@gmail.com>
2026-02-16 11:33:32 +0300

    populate dashboard skeleton, update admin registration

--- backend/mainapp/admin.py
+++ backend/mainapp/admin.py
@@ -1,9 +1,31 @@
 from django.contrib import admin
 from .models import LeadershipTeam, AgeGroup, DiscipleshipGroup, RopesClass, ServingTeam, MinistryData
 # Register your models here.
-admin.site.register(LeadershipTeam)
-admin.site.register(AgeGroup)
-admin.site.register(DiscipleshipGroup)
-admin.site.register(RopesClass)
-admin.site.register(ServingTeam)
-admin.site.register(MinistryData)
+@admin.register(AgeGroup)
+class AgeGroupAdmin(admin.ModelAdmin):
+    list_display = ('name', 'pastor')
+    filter_horizontal = ('members',)  # nice multi-select for members
+
+@admin.register(DiscipleshipGroup)
+class DiscipleshipGroupAdmin(admin.ModelAdmin):
+    list_display = ('name', 'leader')
+    filter_horizontal = ('members',)
+
+@admin.register(LeadershipTeam)
+class LeadershipTeamAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    filter_horizontal = ('members',)
+
+@admin.register(RopesClass)
+class RopesClassAdmin(admin.ModelAdmin):
+    filter_horizontal = ('members',)
+
+@admin.register(ServingTeam)
+class ServingTeamAdmin(admin.ModelAdmin):
+    list_display = ('name', 'leader')
+    filter_horizontal = ('members',)
+
+@admin.register(MinistryData)
+class MinistryDataAdmin(admin.ModelAdmin):
+    list_display = ('user', 'dg')
+    filter_horizontal = ('department',)
