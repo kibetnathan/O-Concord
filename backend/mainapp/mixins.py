e7ddae626c54062689107a609af13263c6588d3a
kibetnathan <kibetnat@gmail.com>
2026-02-17 11:52:38 +0300

    create context mixin

--- backend/mainapp/mixins.py
+++ backend/mainapp/mixins.py
@@ -1,5 +1,7 @@
 # mainapp/mixins.py
 from django.contrib.auth.mixins import UserPassesTestMixin
+from communication.models import Post
+from mainapp.models import DiscipleshipGroup, AgeGroup, ServingTeam, RopesClass
 
 class PastorRequiredMixin(UserPassesTestMixin):
     def test_func(self):
@@ -8,3 +10,16 @@ class PastorRequiredMixin(UserPassesTestMixin):
     def handle_no_permission(self):
         from django.shortcuts import redirect
         return redirect("/not-authorized/")
+
+
+class PastorContextMixin:
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            "post_instances": Post.objects.all()[:5],  # latest 5 posts
+            "total_dgs": DiscipleshipGroup.objects.count(),
+            "age_groups": AgeGroup.objects.count(),
+            "departments": ServingTeam.objects.count(),
+            "ropes_classes": RopesClass.objects.count(),
+        })
+        return context
