a3afef838ff9295f9822076101f0de989f4ece5c
kibetnathan <kibetnat@gmail.com>
2026-02-18 12:56:37 +0300

    add likes to posts

--- backend/communication/models.py
+++ backend/communication/models.py
@@ -9,7 +9,7 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
     published_date = models.DateTimeField(blank=True, null=True)
-
+    liked_by = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name='liked_posts')
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE
