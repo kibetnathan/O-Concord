7b54be5e01e925df183aa65bde46b3f820b0cd3a
kibetnathan <kibetnat@gmail.com>
2026-02-21 14:21:23 +0300

    update user api view

--- backend/userapp/views.py
+++ backend/userapp/views.py
@@ -97,6 +97,8 @@ class CurrentUserAPIView(APIView):
         return Response({
             "id": user.id,
             "username": user.username,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
             "email": user.email,
         })
 
